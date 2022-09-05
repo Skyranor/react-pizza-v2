0b5c8728a2ee5fbe8bea43ff561cb10c7d651af6
Dmitry Baryshev <dima.baryshev@live.com>
2022-09-05 13:50:52 +0300

    add getting pizza json from backend

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import Header from './component/Header';
 import Sort from './component/Sort';
 import Categories from './component/Categories';
 import PizzaBlock from './component/PizzaBlock';
-import pizzas from './assets/img/pizzas.json';
 
 import './scss/app.scss';
-console.log(pizzas);
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://6315a6a55b85ba9b11e3fc35.mockapi.io/items');
+      const json = await response.json();
+      setPizzas(json);
+    })();
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories />
+            {/* <Categories />  */}
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
